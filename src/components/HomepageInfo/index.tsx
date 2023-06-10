ceb32d0c4b9385e63277c90388f9f547672751cd
Ian Webster <ianw_github@ianww.com>
2023-06-10 15:41:45 -0700

    fix issue with overridden style

--- src/components/HomepageInfo/index.tsx
+++ src/components/HomepageInfo/index.tsx
@@ -8,25 +8,11 @@ type FeatureItem = {
   description: JSX.Element;
 };
 
-function Feature({title, Svg, description}: FeatureItem) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
-}
-
 export default function HomepageInfo(): JSX.Element {
   return (
     <section className={styles.section}>
       <div className="container">
-        <h2>
+        <h2 className={styles.blurb}>
           Find the best prompts &amp; prevent regressions by systematically testing for quality.
         </h2>
       </div>
